7cca88c7202d1bafddeded9af76fdd7742ae3b72
Vasyl Zhepikov <42916679+Folongton@users.noreply.github.com>
2023-10-04 21:09:41 -0500

    Update install_vscode.sh

--- install_vscode.sh
+++ install_vscode.sh
@@ -3,7 +3,7 @@
 # /home/ec2-user/anaconda3/envs/JupyterSystemEnv/bin/pip install -U keytar jupyter-server-proxy
 echo == INSTALLING CODE-SERVER ==
 export HOME=/home/ec2-user
-curl -fsSL https://code-server.dev/install.sh | sh -s -- --version=4.14.1  # https://github.com/coder/code-server/releases
+curl -fsSL https://code-server.dev/install.sh | sh -s -- --version=4.17.1  # https://github.com/coder/code-server/releases
 
 #########################################
 ### INTEGRATE CODE-SERVER WITH JUPYTER
